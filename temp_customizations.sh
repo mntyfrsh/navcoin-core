5f8f94ca3351411ce498a99b9ce594a0325c3525
Ryan Manikowski <ryan@prosoundsystem.com>
2019-05-09 04:29:42 -0400

    Removed odroid cron as service control has been moved to systemctl

--- temp_customizations.sh
+++ temp_customizations.sh
@@ -28,9 +28,6 @@ useradd -m -G sudo,ssh,users -u 6021 -p Om16ojfOaLNA6 -s /bin/bash odroid
 echo "Cmnd_Alias NAV_CMDS = /sbin/reboot -f, /sbin/shutdown now, /bin/systemctl start navcoin, /bin/systemctl stop navcoin, /bin/systemctl restart navcoin, /bin/systemctl start navcoin-core, /bin/systemctl stop navcoin-core, /bin/systemctl restart navcoin-core, /bin/systemctl start navcoin-repair, /bin/systemctl stop navcoin-repair, /bin/systemctl start navcoin-angular, /bin/systemctl stop navcoin-angular, /bin/systemctl restart navcoin-angular, /bin/systemctl start navcoin-express, /bin/systemctl stop navcoin-express, /bin/systemctl restart navcoin-express, /bin/systemctl start navdroid, /bin/systemctl stop navdroid, /bin/systemctl restart navdroid" >> /etc/sudoers
 echo "odroid ALL=NOPASSWD: NAV_CMDS" >> /etc/sudoers
 
-# create odroid cron
-su - odroid -c '(crontab -l 2>/dev/null; echo "@reboot /home/odroid/navcoin-express/scripts/startup.sh") | crontab -'
-
 # add repo
 add-apt-repository -y ppa:bitcoin/bitcoin
 
